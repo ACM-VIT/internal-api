5e1e522061005fd3b7789770cbd8d2065fca6297
Vatsal Joshi <vatz88@gmail.com>
2016-07-06 17:14:22 +0530

    Nav for 'Make Announcement' and 'New Event' added to Dashboard.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,37 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+                <div class="col-xs-12">
+                    <ul class="nav nav-tabs">
+                        <li role="presentation" data-toggle="collapse" data-target="#makeAnnouncement"><a href="#">Make
+                                Announcement</a></li>
+                        <li role="presentation" data-toggle="collapse" data-target="#createEvent"><a href="#">New
+                                Event</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 collapse" id="makeAnnouncement">
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="announcementSubject">Subject</label>
+                            <input type="text" id="announcementSubject" class="form-control">
+                            <label for="announcementDetails">Details</label>
+                            <textarea rows="8" class="form-control" id="announcementDetails"></textarea>
+                            <button type="submit" class="btn btn-default">Post Announcement</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-xs-12 collapse" id="createEvent">
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="eventName">Event Name</label>
+                            <input type="text" id="eventName" class="form-control">
+                            <label for="eventDate">Event Date</label>
+                            <input type="date" id="eventDate" class="form-control">
+                            <label for="eventDetails">Details</label>
+                            <textarea rows="8" class="form-control" id="eventDetails"></textarea>
+                            <button type="submit" class="btn btn-default">Create Event</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
